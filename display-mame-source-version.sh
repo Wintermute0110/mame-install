55b87e464f5c5e1275423205a24da5144aedd471
Wintermute0110 <wintermute0110@gmail.com>
2019-11-02 20:36:43 +0900

    Print MAME source directory.

--- display-mame-source-version.sh
+++ display-mame-source-version.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Everytime branches are switched with checkout mame does a clean of the project. Be aware!
 #
+mame_source_dir=/home/kodi/mame-source/
 current_dir=`pwd`
 echo "Current directory $current_dir"
-git -C /home/kodi/mame-source/ status
+echo "MAME source directory $mame_source_dir"
+git -C $mame_source_dir status
