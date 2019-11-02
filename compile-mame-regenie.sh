0ab5e089bf826b8dbe7561aee97f5a5cc4b7755f
Wintermute0110 <wintermute0110@gmail.com>
2019-11-02 22:03:20 +0900

    Improve MAME compilation options.

--- compile-mame-regenie.sh
+++ compile-mame-regenie.sh
@@ -2,6 +2,8 @@
 # For info about MAME compilation see http://docs.mamedev.org/
 # It is safe to call this script multiple times (build is not cleaned).
 # If mame source code is updated/pulled with git then the build is cleaned.
+# https://docs.mamedev.org/initialsetup/compilingmame.html#useful-options
+# https://github.com/mamedev/mame/issues/5161
 
 # Go to MAME source directory
 current_dir=`pwd`
@@ -12,7 +14,7 @@ cd /home/kodi/mame-source/
 num_proc=`nproc`
 echo "Compile using $num_proc processors"
 echo "Regenerating MAME configuration"
-make REGENIE=1 -j$num_proc
+make REGENIE=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 STRIP_SYMBOLS=1 -j$num_proc
 
 # Return to scripts directory
 cd $current_dir
--- compile-mame.sh
+++ compile-mame.sh
@@ -9,7 +9,7 @@ cd /home/kodi/mame-source/
 # Compile MAME. Detect number of available CPU cores.
 num_proc=`nproc`
 echo "Compile using $num_proc processors"
-make -j$num_proc
+make PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 STRIP_SYMBOLS=1 -j$num_proc
 
 # Return to scripts directory
 cd $current_dir
