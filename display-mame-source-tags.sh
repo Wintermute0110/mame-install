78462e794c1a6b17346ffc36139e89c1a4501ec4
Wintermute0110 <wintermute0110@gmail.com>
2020-02-23 21:19:42 +0900

    New utility files.

---
+++ display-mame-source-tags.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Everytime branches are switched with checkout mame does a clean of the project. Be aware!
+#
+mame_source_dir=/home/kodi/mame-source/
+current_dir=`pwd`
+echo "Current directory $current_dir"
+echo "MAME source directory $mame_source_dir"
+git -C $mame_source_dir tag
