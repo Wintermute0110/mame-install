c854c24d3b1dba32ef092a23ea96619757a9bfa4
Wintermute0110 <wintermute0110@gmail.com>
2020-03-14 17:19:39 +0900

    Update to MAME 0.219

--- set-mame-source-version.sh
+++ set-mame-source-version.sh
@@ -5,4 +5,4 @@ mame_source_dir=/home/kodi/mame-source/
 current_dir=`pwd`
 echo "Current directory $current_dir"
 echo "MAME source directory $mame_source_dir"
-git -C $mame_source_dir checkout mame0218
+git -C $mame_source_dir checkout mame0219
