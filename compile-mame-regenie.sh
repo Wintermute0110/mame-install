30473259d1ffe9eb2877528f94928f43bd8b5618
Wintermute0110 <wintermute0110@gmail.com>
2020-06-08 20:25:32 +0900

    Fix compilation in Ubuntu Focal Fossa.

--- compile-mame-regenie.sh
+++ compile-mame-regenie.sh
@@ -4,6 +4,7 @@
 # If mame source code is updated/pulled with git then the build is cleaned.
 # https://docs.mamedev.org/initialsetup/compilingmame.html#useful-options
 # https://github.com/mamedev/mame/issues/5161
+# https://github.com/mamedev/mame/issues/4953
 
 # Go to MAME source directory
 current_dir=`pwd`
@@ -14,7 +15,7 @@ cd /home/kodi/mame-source/
 num_proc=`nproc`
 echo "Compile using $num_proc processors"
 echo "Regenerating MAME configuration"
-make REGENIE=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 STRIP_SYMBOLS=1 -j$num_proc
+make -j$num_proc REGENIE=1 PTR64=1 OPTIMIZE=3 SYMBOLS=1 SYMLEVEL=1 STRIP_SYMBOLS=1 TOOLS=1 ARCHOPTS=-U_FORTIFY_SOURCE
 
 # Return to scripts directory
 cd $current_dir
